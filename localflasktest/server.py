43e2c1a2f460e89092d82a8268e37dd1d4760133
ddefordWSU <71429222+ddefordWSU@users.noreply.github.com>
2021-03-15 19:14:56 -0700

    add I stats

--- localflasktest/server.py
+++ localflasktest/server.py
@@ -60,6 +60,56 @@ def submit2():
         data.writerow([len(matches),userdata["p1"],userdata["p2"],userdata["s1"],userdata["p3"],userdata["p4"],userdata["s2"]])
     return f"Your match has been recorded as match ID #: {len(matches)}"# \n Return to main webpage: {url_for('index')}"
 
-   
+@app.route("/submitI", methods=["GET", "POST"])
+def submit2():
+    if request.method == "GET":
+        return redirect(url_for('index'))
+    elif request.method == "POST":
+        with open("./data/matches_test.csv",'r') as f:
+            reader = csv.reader(f)
+            matches = list(reader)
+            
+        with open('data/roster_test.csv') as csv_file:
+            data = csv.reader(csv_file, delimiter=',')
+            roster = {}
+            for row in data:
+                roster[row[0]] = row[1]
+
+        g_dict = {int(x):0 for x in roster.keys()}
+        w_dict = {int(x):0 for x in roster.keys()}
+        p_dict = {int(x):0 for x in roster.keys()}
+
+        g_dict_p = {(int(x),int(y)):0 for x in roster.keys() for y in roster.keys()}
+        w_dict_p = {(int(x),int(y)):0 for x in roster.keys() for y in roster.keys()}
+        p_dict_p = {(int(x),int(y)):0 for x in roster.keys() for y in roster.keys()}
+
+        for game in matches:
+
+            game = [int(x) for x in game]
+
+            game = [game[0],(game[1],game[2],game[3]),(game[4],game[5],game[6])]
+
+            for i in range(2):
+                pair = game[i+1]
+                g_dict[pair[0]] +=1
+                g_dict_p[(pair[0],pair[1])] +=1
+                g_dict_p[(pair[1],pair[0])] +=1
+
+                if pair[2] ==11:
+                    w_dict[pair[0]] +=1
+                    w_dict_p[(pair[0],pair[1])] +=1
+                    p_dict[pair[0]] +=11
+                    p_dict_p[(pair[0],pair[1])] +=11
+                else:
+                    p_dict[pair[0]] += pair[2]
+                    p_dict_p[(pair[0],pair[1])] +=pair[2]
+
+        plist = []
+        for x in roster.keys():
+            if g_dict[int(x)] > 0:
+                plist.append({"name":roster[x],"id":x,"wins":w_dict[int(x)],"points":p_dict[int(x)],"games":g_dict[int(x)]))
+                              
+        return render_template("statsI.html", plist=plist)                          
+
 if __name__ == "__main__":
   app.run()
